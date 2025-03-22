35ea0cb398ba9149aba7341b12ff9e3cc189cf25
vxewiii <wisitkul.0003@gmail.com>
2025-03-22 14:00:47 +0700

    Update index.js

--- index.js
+++ index.js
@@ -34,11 +34,11 @@ class GetImage {
             if (images.length === 1) {
                 const { url, external_asset_path } = images[0];
                 if (url === url1) {
-                    url1 = url.includes("https://i.ibb.co/218TmmFt/1711043371431.jpg") ? url : external_asset_path;
+                    url1 = url.includes("attachments") ? url : external_asset_path;
                     url2 = null;
                 } else if (url === url2) {
                     url1 = null;
-                    url2 = url.includes("https://i.ibb.co/218TmmFt/1711043371431.jpg") ? url : external_asset_path;
+                    url2 = url.includes("attachments") ? url : external_asset_path;
                 }
             } else if (images.length === 2) {
                 const [img1, img2] = images;
